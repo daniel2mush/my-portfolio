b0f7cb4928bc3999b46fbc54262df9997d3f4bce
daniel <daniel2mush@gmail.com>
2025-08-25 07:19:19 +0000

    removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Contact from "@/components/appComponent/contact";
 import HeroSection from "@/components/appComponent/heroSection";
 import Projects from "@/components/appComponent/projects";
 import Skills from "@/components/appComponent/skils";
-import { env } from "../../env";
 
 export default function HomePAge() {
   return (
